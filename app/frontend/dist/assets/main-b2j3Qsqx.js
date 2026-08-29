b82cff91c6a787c0994e1d4516305ce40f0c5ada
Alex <alexds@MacBook-Air-de-Alex.local>
2026-08-29 15:29:56 -0500

    fix: publish production dashboards and navigation

---
+++ app/frontend/dist/assets/main-b2j3Qsqx.js
@@ -0,0 +1,1 @@
+import{j as r}from"./styles-Cikl63ju.js";import{c as t,r as o}from"./client-BwDKCSD0.js";import{H as e}from"./home-BXVdJz70.js";t.createRoot(document.getElementById("root")).render(r.jsx(o.StrictMode,{children:r.jsx(e,{})}));
